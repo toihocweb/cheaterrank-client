4838731dc689b298d22745f90127b17449e7a8c3
toihocweb <buidinhnhat2031@gmail.com>
2020-07-29 13:49:57 +0700

    #add new api

--- src/saga/getResults.js
+++ src/saga/getResults.js
@@ -1,10 +1,10 @@
 import Axios from "axios";
-import { apiUrl } from "../utils /api";
+import { serverUrl } from "../utils /api";
 
 export const getResults = async (userCode) => {
   try {
     /*eslint-disable no-eval */
-    let res = await Axios.post(`${apiUrl}/api/test`, userCode);
+    let res = await Axios.post(`${serverUrl}/api/test`, userCode);
 
     if (res.data.includes("Error")) {
       return res.data;
--- src/saga/getTests.js
+++ src/saga/getTests.js
@@ -2,6 +2,6 @@ import Axios from "axios";
 import { apiUrl } from "../utils /api";
 
 export const getTests = async () => {
-  const res = await Axios.get(`${apiUrl}/api/cheaterrank/tests`);
+  const res = await Axios.get(`${apiUrl}/api/v1/cheaterrank/tests`);
   return res.data;
 };
