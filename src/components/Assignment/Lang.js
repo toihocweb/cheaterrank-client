fd466c58ba47b23c1916a6cf684e111633a3f4ce
toihocweb <buidinhnhat2031@gmail.com>
2020-08-07 05:39:46 +0700

    #add proxy socket

--- src/components/Assignment/Lang.js
+++ src/components/Assignment/Lang.js
@@ -29,7 +29,7 @@ const Lang = () => {
 
   useEffect(() => {
     // const socket = openSocket("https://toihocweb.net/socket.io/");
-    const socket = openSocket("https://127.0.0.1:8000", { secure: true });
+    const socket = openSocket("http://202.182.100.160:8000", { secure: true });
     socket.emit("user", {
       id: currentUserFromStore.id,
       name: currentUserFromStore.name,
