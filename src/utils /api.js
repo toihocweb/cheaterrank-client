d36033556c505cc3989f9b1617ef3dd4e4abfa13
toihocweb <44312672+toihocweb@users.noreply.github.com>
2021-04-18 14:05:37 +0700

    Delete api.js

--- src/utils /api.js
+++
@@ -1,9 +0,0 @@
-export const apiUrl =
-  process.env.REACT_APP_ENV === "prod"
-    ? "http://toihocweb.net"
-    : "http://localhost:8000";
-
-export const serverUrl =
-  process.env.REACT_APP_ENV === "prod"
-    ? "http://toihocweb.net"
-    : "http://localhost:5000";
